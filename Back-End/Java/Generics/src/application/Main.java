d072b655dcaab715a00e14f881249c0e14e08539
Nickolas Alves <nickolaschang@hotmail.com>
2022-11-11 11:31:33 +1100

    Generics Bounded Wildcards types

--- Back-End/Java/Generics/src/application/Main.java
+++ Back-End/Java/Generics/src/application/Main.java
@@ -1,5 +1,8 @@
 package application;
 
+import entities.*;
+
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -8,35 +11,29 @@ public class Main {
 	
 	public static void main(String[] args) {
 		
-		/*
-		 * List<Object>obj = new ArrayList<>();
-		 * List<Integer>number = new ArrayList<>();
-		 * obj = number;
-		 *
-		 * Compilation error, a List of Object can contain Integer, but a List of Integer cannot be a List of Object.
-		 */
+		List<Shape> myShapes = new ArrayList<>();
+		myShapes.add(new Rectangle(3, 2));
+		myShapes.add(new Circle(2.0));
 		
-		/*
-		 * List<?>obj = new ArrayList<>();
-		 * List<Integer>number = new ArrayList<>();
-		 * obj = number;
-		 *
-		 * This works because we are now using the wildcard generic type <?>
-		 */
+		List<Circle> myCircles = new ArrayList<>();
+		myCircles.add(new Circle(2));
+		myCircles.add(new Circle(3));
 		
-		List<Integer> myInts = Arrays.asList(5, 2, 10);
-		printList(myInts);
+		List<Rectangle> myRectangles = new ArrayList<>();
+		myRectangles.add(new Rectangle(2, 6));
+		myRectangles.add(new Rectangle(3, 8));
 		
-		List<String> myStrs = Arrays.asList("Alex", "Maria", "Bob");
-		printList(myStrs);
+		
+		System.out.println("Total area Shapes: " + String.format("%.2f", totalArea(myShapes)));
+		System.out.println("Total area Circles: " + String.format("%.2f", totalArea(myCircles)));
+		System.out.println("Total area Rectangles: " + String.format("%.2f", totalArea(myRectangles)));
 	}
 	
-	public static void printList(List<?> list) {
-		// list.add(3) <- remove // to test
-		// wouldn't work, because the compiler cannot know what type of list it is going
-		// to be added (<?> type), therefore it catches the error beforehand.
-		for (Object obj : list) {
-			System.out.println(obj);
+	public static double totalArea(List<? extends Shape> list) {
+		double sum = 0;
+		for (Shape shape : list) {
+			sum += shape.area();
 		}
+		return sum;
 	}
 }
