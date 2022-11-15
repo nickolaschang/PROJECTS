8f2c0e212ac4b05a0950cebccebb13385bc3380c
Nickolas Alves <nickolaschang@hotmail.com>
2022-11-15 18:43:00 +1100

    First Component

---
+++ website-project/DSMETA/frontend/src/components/NotificationButton/index.tsx
@@ -0,0 +1,12 @@
+import icon from '../../assets/img/notification-icon.svg'
+import './styles.css'
+
+function NotificationButton() {
+    return (
+        <div className="DSMeta-notify-button">
+            <img src= {icon} alt="Notify" />
+        </div>
+    )
+}
+
+export default NotificationButton
