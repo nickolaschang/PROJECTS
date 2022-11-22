adf5fd2c57f359486b2d37cb3523406de30780d4
Nickolas Alves <nickolaschang@hotmail.com>
2022-11-22 13:10:39 +1100

    SMS implementation

--- website-project/DSMETA/backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ website-project/DSMETA/backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -14,6 +14,7 @@ import com.devsuperior.dsmeta.entities.Sale;
 import com.devsuperior.dsmeta.repositories.SaleRepository;
 
 
+
 @Service
 @Repository
 public class SaleService {
---
+++ website-project/DSMETA/backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
@@ -0,0 +1,45 @@
+package com.devsuperior.dsmeta.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.devsuperior.dsmeta.entities.Sale;
+import com.devsuperior.dsmeta.repositories.SaleRepository;
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+
+@Service
+public class SmsService {
+
+	@Value("${twilio.sid}")
+	private String twilioSid;
+
+	@Value("${twilio.key}")
+	private String twilioKey;
+
+	@Value("${twilio.phone.from}")
+	private String twilioPhoneFrom;
+
+	@Value("${twilio.phone.to}")
+	private String twilioPhoneTo;
+
+	@Autowired
+	private SaleRepository saleRepository;
+	
+	public void sendSms(Long orderId) {
+		Twilio.init(twilioSid, twilioKey);
+		
+		Sale sale = saleRepository.findById(orderId).get();
+		
+		String msg = "Hello there " + sale.getSellerName()+ " Carlingford Music Centre here, hope you are doing fine! Your order number: " + sale.getId() + " that you finished in: " + sale.getDate();
+		PhoneNumber to = new PhoneNumber(twilioPhoneTo);
+		PhoneNumber from = new PhoneNumber(twilioPhoneFrom);
+
+		Message message = Message.creator(to, from, msg + " is on the way! Use this track number to see where it is: CMC900023718CF").create();
+
+		System.out.println(message.getSid());
+	}
+
+}
